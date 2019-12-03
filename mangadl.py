5fcc8cbc70ea90a0a8636f237ad7aad0a7b4584f
jhaco <50258964+jhaco@users.noreply.github.com>
2019-12-02 19:01:44 -0600

    Added exception handling

--- mangadl.py
+++ mangadl.py
@@ -16,19 +16,22 @@ def get_chapters(site):
 
 def dl_page(site, directory):
     page = requests.get(site)
-    html = page.text
-    pattern = re.compile(r'<\s*img [^>]*src="([^"]+)')                          
-    img_url = pattern.findall(html)                                         #finds all html code for source images
+    if(page.ok):
+        html = page.text
+        pattern = re.compile(r'<\s*img [^>]*src="([^"]+)')                          
+        img_url = pattern.findall(html)                                         #finds all html code for source images
 
-    for img in img_url:
-        #===================================================================
-        filename = re.search(r'/([\w_-]+[.](jpg))$', img)                   #applies to jpg only; file type for comic pages; MODIFY FOR DIFFERENT IMAGE TYPE
-        #===================================================================
-        if filename is None:                                                #skips non-jpg image files i.e. headers, etc
-            continue
-        with open(os.path.join(directory, filename.group(1)), 'wb') as f:
-            response = requests.get(img)                                    #requests img url, extracts image and writes to file
-            f.write(response.content)
+        for img in img_url:
+            #===================================================================
+            filename = re.search(r'/([\w_-]+[.](jpg))$', img)                   #applies to jpg only; file type for comic pages; MODIFY FOR DIFFERENT IMAGE TYPE
+            #===================================================================
+            if filename is None:                                                #skips non-jpg image files i.e. headers, etc
+                continue
+            with open(os.path.join(directory, filename.group(1)), 'wb') as f:
+                response = requests.get(img)                                    #requests img url, extracts image and writes to file
+                f.write(response.content)
+    else:
+        print("Failed to get: " + site)
 
 
 #===========================================================================
@@ -36,15 +39,19 @@ def dl_page(site, directory):
 start_time = time.time()
 
 site = input("Enter the site: ")
-chapters = get_chapters(site) 
+try:
+    chapters = get_chapters(site) 
 
-for chap in chapters:                                                       
-    name = chap.split("/")[-1]                                              #assumes chapter number is appended at end of chapter url
-    number = re.findall(r'[\d\.\d]+', name)                                 #removes all other non-numeric characters
-    directory = os.path.dirname(os.path.realpath(__file__)) + '/' + number[-1]
-    if not os.path.exists(directory):
-        os.makedirs(directory)                                              #generates folder for each chapter
-    dl_page(chap, directory)
+    for chap in chapters:                                                       
+        name = chap.split("/")[-1]                                              #assumes chapter number is appended at end of chapter url
+        number = re.findall(r'[\d\.\d]+', name)                                 #removes all other non-numeric characters
+        directory = os.path.dirname(os.path.realpath(__file__)) + '/' + number[-1]
+        if not os.path.exists(directory):
+            os.makedirs(directory)                                              #generates folder for each chapter
+        dl_page(chap, directory)
+
+except:
+    print("Invalid URL")
 
 end_time = time.time()
 print("Elapsed time was %g seconds" % (end_time - start_time)) 
