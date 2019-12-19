76587ecf6c3f033a3baffee32207f291fcebfb0a
jhaco <50258964+jhaco@users.noreply.github.com>
2019-12-19 03:23:59 -0600

    Add files via upload

---
+++ run_mangadl.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+
+python "%~dp0\mangadl.py" "https://manganelo.com/manga/peerless_dad" "Peerless_Dad"
+ECHO completed: Peerless Dad & ECHO.
+
+python "%~dp0\mangadl.py" "https://manganelo.com/manga/pn918005" "Solo_Leveling"
+ECHO completed: Solo Leveling & ECHO.
+PAUSE
